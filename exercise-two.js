ee1e70e2420ee68c9a4daf1ead038c008291cb85
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-05 23:06:03 +0200

    Add the second exercise solution

--- exercise-two.js
+++ exercise-two.js
@@ -1,3 +1,7 @@
 /**
  * Write a JavaScript exercise to get the extension of a filename.
  */
+const getExtension = (str) => str.slice(str.lastIndexOf("."));
+
+console.log(getExtension("index.html"));
+console.log(getExtension("index.css"));
