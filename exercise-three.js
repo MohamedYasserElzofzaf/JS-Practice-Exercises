ab12354b7912085850aaf73b8d83dac3bdc99348
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-05 23:19:07 +0200

    Add the third exercise solution

--- exercise-three.js
+++ exercise-three.js
@@ -3,3 +3,11 @@
  * replace every character in a given string
  * with the character following it in the alphabet
  */
+
+const moveChars = (str) =>
+    str
+    .split("")
+    .map((char) => String.fromCharCode(char.charCodeAt(0) + 1))
+    .join("");
+
+console.log(moveChars("amdfj"));
