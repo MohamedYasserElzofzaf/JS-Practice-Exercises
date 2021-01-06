dbd69abeed2aee009c50d69e0fcc5298314a3b24
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-06 22:32:07 +0200

    Add the six exercise solution

--- exercise-6.js
+++ exercise-6.js
@@ -4,3 +4,9 @@
  *  The string length must be 3 or more,
  *  if not, the original string is returned.
  */
+
+const makeString = (str) =>
+    str.length < 3 ? str : str.slice(0, 3) + str.slice(-3);
+
+console.log(makeString("mohammed"));
+console.log(makeString("yasser"));
