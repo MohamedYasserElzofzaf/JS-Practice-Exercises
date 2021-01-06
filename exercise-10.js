4b458a6bd47ef5a7a4588f7682409e8b41488f94
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-06 23:19:04 +0200

    Add the exercise number 10 's solution

--- exercise-10.js
+++ exercise-10.js
@@ -2,3 +2,13 @@
  * Write a JavaScript program
  * to check a given string contains 2 to 4 occurrences of a specified character.
  */
+
+const countChars = (str, char) =>
+    str.split("").filter((ch) => ch === char).length;
+
+const contains2to4 = (str, char) =>
+    countChars(str, char) >= 2 && countChars(str, char) <= 4;
+
+console.log(contains2to4("oooooh", "o"));
+console.log(contains2to4("ooooh", "o"));
+console.log(contains2to4("oooh", "o"));
