587b4d09782073755299f4bbf781ddaab01522fc
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-06 22:44:03 +0200

    Add the 7 exercise solution

--- exercise-7.js
+++ exercise-7.js
@@ -1,3 +1,7 @@
 /** Write a JavaScript program
  * to extract the first half of a string
  *  of even length.*/
+
+const halfString = (str) => str.slice(0, str.length / 2);
+
+console.log(halfString("mohammed"));
