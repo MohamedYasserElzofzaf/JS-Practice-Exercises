1b00b96a898875d1f174cb38d90cf8ece35607f6
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-06 23:07:31 +0200

    Add the 9 exercise solution

--- exercise-9.js
+++ exercise-9.js
@@ -3,3 +3,7 @@
  *  write a JavaScript program to
  *  find out which one is nearest to 100
  */
+
+const near100 = (a, b) => (a < b ? b : a);
+console.log(near100(45, 90));
+console.log(near100(95, 90));
