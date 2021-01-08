11de69244e92304a6bfafa16908b10b93ebd4af4
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-08 23:55:59 +0200

    Add the exercise number 15's solution

--- exercise-15.js
+++ exercise-15.js
@@ -3,3 +3,9 @@
  * replace the first digit in a string (should contain at least one digit)
  * with a $ character.
  */
+
+const replaceFirstDigit = (str) => str.replace(/[0-9]/, "$");
+
+console.log(replaceFirstDigit("asgyy3274hjndb"));
+console.log(replaceFirstDigit("1232429kcdjn"));
+console.log(replaceFirstDigit("A23dfdn32"));
