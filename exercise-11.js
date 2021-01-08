e6503caf1aa52a2d469c7e48c7acecf521173cce
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-08 22:31:02 +0200

    Add the exercise number 11 's solution

--- exercise-11.js
+++ exercise-11.js
@@ -2,3 +2,20 @@
  * Write a JavaScript program to
  * find the number of even digits in a an array of integers
  */
+
+// method
+const countEvenNumbers = (arr) => arr.filter((num) => num % 2 === 0).length;
+
+// another method
+const findeven = (arr) => {
+    let counter = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            counter++;
+        }
+    }
+    return counter;
+};
+
+console.log(findeven([23, 74, 344, 22, 8, 67]));
+console.log(countEvenNumbers([23, 74, 344, 22, 8, 67]));
