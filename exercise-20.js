9f36a99e2efb7d565bbf35eda5756ae654b0118f
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-11 00:08:05 +0200

    Add the exercise number 20's solution

--- exercise-20.js
+++ exercise-20.js
@@ -3,3 +3,12 @@
  * returns true if the provided predicate function returns true for all elements in a collection,
  * false otherwise.
  */
+
+const isEveryElement = (arr, fn) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (!fn(arr[i])) return false;
+    }
+    return true;
+};
+
+console.log(isEveryElement([1, 2, 3, 4, 5], (x) => x > 0));
