58fb4dce11386a3378349b66a33bb121a834a18f
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-10 21:57:37 +0200

    Add the exercise number 17

---
+++ exercise-17.js
@@ -0,0 +1,9 @@
+/**
+ * Write a JavaScript program to
+ * compare two objects to determine if the first one contains the same properties as the second one
+ * (which may also have additional properties).
+ * For example, objA and objB are equal (but not equal to objC).
+ * const objA = { a: 1, b: 1, c: 1 };
+ * const objB = { a: 1, b: 1, c: 1 };
+ * const objC = { a: 1, b: 1, d: 1 };
+ */
