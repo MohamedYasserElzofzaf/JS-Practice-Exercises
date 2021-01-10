06e64a951f2c9fc9a47a3369f8f5d9ba526bba60
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-10 21:49:32 +0200

    Add the exercise number 16

---
+++ exercise-16.js
@@ -0,0 +1,6 @@
+/**
+ * Given a year, report if it is a leap year.
+ * what's a leap year ?
+ * a year, occurring once every four years,
+ * which has 366 days including 29 February as an intercalary day.
+ */
