89c3a1e961cef8210eee95de417d765f36cdb97d
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-10 21:55:40 +0200

    Add the exercise number 16's solution

--- exercise-16.js
+++ exercise-16.js
@@ -4,3 +4,11 @@
  * a year, occurring once every four years,
  * which has 366 days including 29 February as an intercalary day.
  */
+
+const isItLeapYear = (num) => (num % 4 === 0 ? true : false);
+
+console.log(isItLeapYear(2014));
+console.log(isItLeapYear(2015));
+console.log(isItLeapYear(2016));
+console.log(isItLeapYear(2017));
+console.log(isItLeapYear(2020));
