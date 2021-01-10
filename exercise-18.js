e7263dd4cd9f4943526a7452b3cc9d509ae2f626
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-10 22:15:55 +0200

    Add the exercise number 18

---
+++ exercise-18.js
@@ -0,0 +1,8 @@
+/**
+ * Write a JavaScript program
+ * to convert a comma-separated values (CSV) string to a 2D array.
+ * A new line indicates a new row in the array. Example input:
+ * abc,def,ghi
+ * jkl,mno,pqr
+ * stu,vwx,yza
+ */
